b97454d9a904f1b025a1dc763a28ce3e11fb9c5f
Gabriel Matte <gabrielmatte@msn.com>
2024-12-09 21:34:23 -0500

    Adds logs back in docker

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -6,5 +6,10 @@ envsubst '${PORT} ${FRONTEND_HOST} ${FRONTEND_PORT} ${BACKEND_HOST} ${BACKEND_PO
     < /etc/nginx/templates/default.conf \
     > /etc/nginx/conf.d/default.conf
 
+# Adds logs for docker
+ln -sf /dev/stdout /var/log/nginx/access.log
+ln -sf /dev/stderr /var/log/nginx/error.log
+ln -sf /dev/stderr /var/log/nginx/debug.log
+
 # Start nginx
 exec nginx -g "daemon off;"
