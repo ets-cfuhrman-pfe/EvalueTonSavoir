d62b398de7c9114ffeabbe75b3fa451e13dc09be
C. Fuhrman <christopher.fuhrman@etsmtl.ca>
2025-02-14 21:27:00 -0500

    Generate error for unsupported GIFT types labeled SVGs for easier testing removed templates for unsupported GIFT types

--- client/src/components/GiftTemplate/index.ts
+++ client/src/components/GiftTemplate/index.ts
@@ -18,11 +18,11 @@ What is the capital of Canada? {=Canada -> Ottawa =Italy -> Rome =Japan -> Tokyo
 
 
 const items = multiple.map((item) => Template(item, { theme: 'dark' })).join('');
-const errorItemDark = ErrorTemplate('Hello');
+const errorItemDark = ErrorTemplate('Hello', 'Error');
 
 const lightItems = multiple.map((item) => Template(item, { theme: 'light' })).join('');
 
-const errorItem = ErrorTemplate('Hello');
+const errorItem = ErrorTemplate('Hello', 'Error');
 
 const app = document.getElementById('app');
 if (app) app.innerHTML = items + errorItemDark + lightItems + errorItem;
