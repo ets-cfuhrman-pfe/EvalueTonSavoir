c175bbbc0f2a4633f95564b171e942ea98f1b8b1
Minh-Khoi-Le <minh-khoi.le.1@ens.etsmtl.ca>
2025-12-17 13:27:50 -0500

    Remove css import and update styling

--- client/src/pages/AuthManager/AuthDrawer.tsx
+++ client/src/pages/AuthManager/AuthDrawer.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import './authDrawer.css';
+import { Link } from 'react-router-dom';
 import SimpleLogin from './providers/SimpleLogin/Login';
 import authService from '../../services/AuthService';
 import { ENV_VARIABLES } from '../../constants';
@@ -8,7 +7,6 @@ import ButtonAuth from './providers/OAuth-Oidc/ButtonAuth';
 
 const AuthSelection: React.FC = () => {
     const [authData, setAuthData] = useState<any>(null); // Stocke les données d'auth
-    const navigate = useNavigate();
 
     ENV_VARIABLES.VITE_BACKEND_URL;
     // Récupérer les données d'authentification depuis l'API
@@ -22,37 +20,39 @@ const AuthSelection: React.FC = () => {
     }, []);
 
     return (
-        <div className="auth-selection-page">
-            <h1>Connexion</h1>
+        <div className="login-container">
+            <h1 className="title">Connexion</h1>
 
-            {/* Formulaire de connexion Simple Login */}
-            {authData && authData['simpleauth'] && (
-                <div className="form-container">
+            <div className="inputs">
+                <img className="avatar" src="./people.svg" alt="Avatar"></img>
+
+                {/* Formulaire de connexion Simple Login */}
+                {authData && authData['simpleauth'] && (
                     <SimpleLogin />
-                </div>
-            )}
+                )}
 
-            {/* Conteneur OAuth/OIDC */}
-            {authData && Object.keys(authData).some(key => authData[key].type === 'oidc' || authData[key].type === 'oauth') && (
-                <div className="auth-button-container">
-                    {Object.keys(authData).map((providerKey) => {
-                        const providerType = authData[providerKey].type;
-                        if (providerType === 'oidc' || providerType === 'oauth') {
-                            return (
-                                <ButtonAuth
-                                    key={providerKey}
-                                    providerName={providerKey}
-                                    providerType={providerType}
-                                />
-                            );
-                        }
-                        return null;
-                    })}
-                </div>
-            )}
+                {/* Conteneur OAuth/OIDC */}
+                {authData && Object.keys(authData).some(key => authData[key].type === 'oidc' || authData[key].type === 'oauth') && (
+                    <div className="auth-button-container">
+                        {Object.keys(authData).map((providerKey) => {
+                            const providerType = authData[providerKey].type;
+                            if (providerType === 'oidc' || providerType === 'oauth') {
+                                return (
+                                    <ButtonAuth
+                                        key={providerKey}
+                                        providerName={providerKey}
+                                        providerType={providerType}
+                                    />
+                                );
+                            }
+                            return null;
+                        })}
+                    </div>
+                )}
+            </div>
 
-            <div>
-                <button className="home-button-container" onClick={() => navigate('/')}>Retour à l'accueil</button>
+            <div className="login-links">
+                <Link to="/">Retour à l'accueil</Link>
             </div>
         </div>
     );
